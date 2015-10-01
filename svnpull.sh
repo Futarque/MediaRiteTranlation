08398ee9031a2790de2f53a11574deb72ffd19fc
Jan Pedersen <jap@futarque.com>
2015-10-01 11:59:46 +0200

    More

--- svnpull.sh
+++ svnpull.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-#svn export https://svn/MediaRite/trunk/Resource/Language/7702/locale
+svn export https://svn/MediaRite/trunk/Resource/Language/7702/locale
 svn export https://svn/MediaRite/trunk/Source/Android/Amoeba/app/src/main/res/values/strings.xml strings.tmp
 cat strings.tmp |grep -v 'translatable="false"' > Amoeba/values/strings.xml
 rm -f strings.tmp
 
-#svn export https://svn/MediaRite/trunk/Source/Android/Amoeba/app/src/main/res/values-da/strings.xml Amoeba/values-da/strings.xml
-#svn export https://svn/MediaRite/trunk/Source/Android/Amoeba/app/src/main/res/values-en/strings.xml Amoeba/values-en/strings.xml
+mkdir -p Amoeba/values-da/
+svn export https://svn/MediaRite/trunk/Source/Android/Amoeba/app/src/main/res/values-da/strings.xml Amoeba/values-da/strings.xml
+mkdir -p Amoeba/values-en/
+svn export https://svn/MediaRite/trunk/Source/Android/Amoeba/app/src/main/res/values-en/strings.xml Amoeba/values-en/strings.xml
+#mkdir -p Amoeba/values-nl/
+#mkdir -p Amoeba/values-it/
+#mkdir -p Amoeba/values-fr/
+#mkdir -p Amoeba/values-pl/
+#mkdir -p Amoeba/values-fi/
+#mkdir -p Amoeba/values-es/
+#mkdir -p Amoeba/values-hu/
+#mkdir -p Amoeba/values-nb/
+#mkdir -p Amoeba/values-ro/
+#mkdir -p Amoeba/values-sv/
+#mkdir -p Amoeba/values-de/
 
--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CMD=ls
+CMD=echo
 SDIR=.
 TDIR=.
 
@@ -8,8 +8,8 @@ function help() {
 	echo "[options] <rootOfMediaRite>"	
 	echo "-push      copy TO MediaRite"
 	echo "-update    copy FROM MediaRite"
-	echo "-m         meld"
-
+	echo "-meld      meld"
+	echo "-echo      echo cmd"
 }
 
 function argerror() {
@@ -18,13 +18,13 @@ function argerror() {
 	exit 1;
 }
 
-EXP_OPT=''
 while [ -n "$1" ]; do
 	case "$1" in
 		-push)
 			shift
 			CMD=cp
 			TDIR=$1
+
 			;;
 		-update)
 			shift
@@ -34,6 +34,11 @@ while [ -n "$1" ]; do
 		-meld)
 			shift
 			CMD=meld
+			TDIR=$1
+			;;
+		-echo)
+			shift
+			CMD=echo
 			TDIR=$1
 			;;
 		--)
@@ -46,10 +51,25 @@ while [ -n "$1" ]; do
 			break;
 			;;
 	esac
-	shift;
-
 done
 
+if [ -z "$1" ] ; then
+	argerror "MediaRite location param missing"
+fi
 
-meld locale/ ${TDIR}/Resource/Language/7702/locale/ &
-meld Amoeba/ $1/Source/Android/Amoeba/app/src/main/res/ &
+if [ "$CMD" == "copy" ] ; then
+$CMD ${SDIR}/Resource/Language/7702/locale/master.pot ${TDIR}/Resource/Language/7702/locale/master.pot
+for dir in ${SDIR}/Resource/Language/7702/locale/; do
+    [ -d "${dir}" ] || continue # if not a directory, skip
+    dirname="$(basename "${dir}")"    
+    $CMD ${SDIR}/Resource/Language/7702/locale/${dirname}/mediarite.po ${TDIR}/Resource/Language/7702/${dirname}/mediarite.po
+done
+for dir in ${SDIR}/Source/Android/Amoeba/app/src/main/res/values*; do
+    [ -d "${dir}" ] || continue # if not a directory, skip
+    dirname="$(basename "${dir}")"    
+    $CMD ${SDIR}/Source/Android/Amoeba/app/src/main/res/${dirname}/strings.xml ${TDIR}/Source/Android/Amoeba/app/src/main/res/${dirname}/strings.xml
+done
+else
+$CMD ${SDIR}/Resource/Language/7702/locale/ ${TDIR}/Resource/Language/7702/locale/
+$CMD ${SDIR}/Source/Android/Amoeba/app/src/main/res/ ${TDIR}/Source/Android/Amoeba/app/src/main/res/
+fi
